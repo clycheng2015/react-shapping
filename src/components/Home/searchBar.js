89cfd896fe272006004cb05e40e3b4f96bf37990
两瓶阿萨姆丶 <vivibearsir@163.com>
2017-10-16 18:12:00 +0800

    修改主页

--- src/components/Home/searchBar.js
+++ src/components/Home/searchBar.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
-const SearchBar = (props) =>  (
+const SearchBar = (props) => {
+
+
+    return(
 
 
         <div className="search-bar">
-
+1312313npm
 
         </div>
     )
+
+}
 
 
 export  default SearchBar
